e3a0abec15ef33dc17c9afe16fc2e1dc2d03942b
Meesum A Qazalbash <mesumali26.ma@gmail.com>
2022-06-28 17:16:25 +0500

    new

--- assignment/mandelbrot-set/mandelbrot-set.js
+++ assignment/mandelbrot-set/mandelbrot-set.js
@@ -1,47 +1,47 @@
 "use strict";
 
 var gl,
-	points = [],
-	z,
-	n = 5;
+    points = [],
+    z,
+    n = 5;
 
 window.onload = function init() {
-	var canvas = document.getElementById("set");
-	gl = WebGLUtils.setupWebGL(canvas);
+    var canvas = document.getElementById("set");
+    gl = WebGLUtils.setupWebGL(canvas);
 
-	if (!gl) {
-		alert("WebGL is not available");
-	}
+    if (!gl) {
+        alert("WebGL is not available");
+    }
 
-	gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.viewport(0, 0, canvas.width, canvas.height);
 
-	for (var i = -2; i <= 2; i += 1 / n) {
-		for (var j = -2; j <= 2; j += 1 / n) {
-			z = point_gen(vec2(0.0, 0.0), vec2(i / n, j / n));
-			points.push(z);
-		}
-	}
+    for (var i = -2; i <= 2; i += 1 / n) {
+        for (var j = -2; j <= 2; j += 1 / n) {
+            z = point_gen(vec2(0.0, 0.0), vec2(i / n, j / n));
+            points.push(z);
+        }
+    }
 
-	console.log(points);
+    console.log(points);
 };
 
 function square(z) {
-	// (a + bi)^2 = a^2 - b^2 + 2abi
-	return vec2(z[0] * z[0] - z[1] * z[1], 2 * z[0] * z[1]);
+    // (a + bi)^2 = a^2 - b^2 + 2abi
+    return vec2(z[0] * z[0] - z[1] * z[1], 2 * z[0] * z[1]);
 }
 
 function norm(z) {
-	return Math.sqrt(z[0] ** 2 + z[1] ** 2);
+    return Math.sqrt(z[0] ** 2 + z[1] ** 2);
 }
 
 function point_gen(z, c, generation = 10) {
-	if (norm(z) <= 2) {
-		if (generation == 0) {
-			return z;
-		}
-		generation--;
-		z = add(square(z), c);
-		return point_gen(z, c, generation);
-	}
-	return vec2(0.0, 0.0);
+    if (norm(z) <= 2) {
+        if (generation == 0) {
+            return z;
+        }
+        generation--;
+        z = add(square(z), c);
+        return point_gen(z, c, generation);
+    }
+    return vec2(0.0, 0.0);
 }
--- minutes.md
+++ minutes.md
@@ -1,3 +1,18 @@
+# June 28, 2022
+
+# June 23, 2022
+
+-   Due to some technical issues, I was unable to attend the meeting. Then sir looked into the code and commented in the code with the his remarks.
+
+# June 21, 2022
+
+-   We discussed the camera and the viewing volume, and how that works in WebGL.
+-   We talk about the different stages of pipeline, prominantly, vetex shader and fragment shader.
+-   We look some of the examples of WebGL program from simple to more complex.
+-   After this Dr Waqar assigned me two tasks:
+    -   Write a simple WebGL program that draws polygons.
+    -   Write a WebGL program that generates and shades a mandelbrot set.
+
 # June 16, 2022
 
 -   Reviewing the [html and JavaScript files](\WebGL_codes\Chap2).
