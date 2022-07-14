76238c75de7033d4bbd4a3de602daad0eb847fb0
Meesum A. Qazalbash <mesumali26.ma@gmail.com>
2022-07-14 16:21:59 +0500

    Some

--- assignment/mandelbrot-set/mandelbrot-set.js
+++ assignment/mandelbrot-set/mandelbrot-set.js
@@ -2,8 +2,8 @@
 
 var gl,
 	t,
-	level = [],
-	n = 5;
+	level = [(2, 1), (1, 1), (1, 2), (2, 2)],
+	n = 10;
 
 window.onload = function init() {
 	var canvas = document.getElementById("set");
@@ -14,8 +14,9 @@ window.onload = function init() {
 	}
 
 	gl.viewport(0, 0, canvas.width, canvas.height);
-	for (var i = -2; i <= 2; i++) {
-		for (var j = -2; j <= 2; j++) {
+	var z;
+	for (var i = -2; i <= 2; i = i + 1 / n) {
+		for (var j = -2; j <= 2; j = j + 1 / n) {
 			level.push(mandelbrot_set(vec2(i, j)));
 		}
 	}
@@ -39,61 +40,25 @@ function mandelbrot_set(c, MAX_ITTER = 100) {
 		z = add(square(z), c);
 		count++;
 	}
-	return count;
+	if (norm(z) > 2) {
+		return vec2(c);
+	} else {
+		return vec2(0.0, 0.0);
+	}
 }
 
 var vSHADER = `attribute vec2 pos;
 	varying vec2 _pos;
 	
 	void main() {
-		gl_Position = vec4(_pos = pos, 0, 1);
+		gl_Position = vec4(_pos = pos, 0.0, 1.0);
 	}`,
 	fSHADER = `precision highp float;
 	varying vec2 _pos;
 
 	void main() {
-	    vec2 z, c = _pos * 1.5 - vec2(0.7, 0);
-	    int i = 0;
-		for(;i < 10000; i++) {
-	        z = vec2(z.x * z.x - z.y * z.y, 2.0 * z.x * z.y) + c;
-	        if (length(z) > 2.0) {
-				break;
-			}
-	    }
-		if (i == 9999){
-		    gl_FragColor = vec4(vec3(0,0,0), 1);
-		}
-		else {
-			gl_FragColor = vec4(vec3((float(i) - log(log(length(z)))) / 64.0), 1);
-		}`;
-
-function draw() {
-	gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
-	program = createProgram(gl, vSHADER, fSHADER);
-
-	gl.useProgram(program);
-
-	// gl.vertexAttribPointer(
-	// 	gl.getAttribLocation(program, "pos"),
-	// 	2,
-	// 	gl.FLOAT,
-	// 	false,
-	// 	0,
-	// 	0
-	// );
-	// gl.drawArrays(gl.TRIANGLES, 0, 6);
-
-	var vBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
-
-	var vPosition = gl.getAttribLocation(program, "pos");
-	gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
-	gl.enableVertexAttribArray(vPosition);
-
-	render();
-}
+		gl_FragColor = vec4(_pos, 1.0, 0.0);
+	}`;
 
 function createProgram(gl, vertexShaderSource, fragmentShaderSource) {
 	var vsh = gl.createShader(gl.VERTEX_SHADER);
@@ -116,6 +81,26 @@ function createProgram(gl, vertexShaderSource, fragmentShaderSource) {
 		throw "Link error in program:  " + gl.getProgramInfoLog(prog);
 	}
 	return prog;
+}
+
+function draw() {
+	gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+	gl.useProgram(program);
+
+	console.log(level);
+
+	var program = createProgram(gl, vSHADER, fSHADER);
+
+	var vBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, flatten(level), gl.STATIC_DRAW);
+
+	var vPosition = gl.getAttribLocation(program, "pos");
+	gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(vPosition);
+
+	render();
 }
 
 function render() {
