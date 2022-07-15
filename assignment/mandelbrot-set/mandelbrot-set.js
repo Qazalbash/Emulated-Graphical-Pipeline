524fdd085f4762179f1d4a1dd98efb0966ed554c
Meesum A. Qazalbash <mesumali26.ma@gmail.com>
2022-07-15 16:20:29 +0500

    All re-written but no thing is showing

--- assignment/mandelbrot-set/mandelbrot-set.js
+++ assignment/mandelbrot-set/mandelbrot-set.js
@@ -5,6 +5,100 @@ var gl,
 
 const MAX_ITERATION = 100;
 
+var vSHADER = `attribute vec2 vPosition;
+			attribute vec4 vColor;
+			varying vec4 g1_vColor;
+
+			void main(){
+				gl_Position = vec4(vPosition, 0.0, 1.0);
+				g1_vColor = vColor;
+				gl_PointSize = 1.0;
+			}`,
+	fSHADER = `precision highp float;
+			uniform vec4 g1_vColor;
+	
+			void main()
+			{
+				gl_FragColor = g1_vColor;
+			}`;
+
+function norm(z) {
+	return Math.sqrt(z.x * z.x + z.y * z.y);
+}
+
+function mandelbrot_set(c) {
+	let count = 0,
+		z = { x: 0.0, y: 0.0 };
+	do {
+		z.x = z.x * z.x - z.y * z.y + c.x;
+		z.y = 2 * z.x * z.y + c.y;
+		count++;
+	} while (norm(z) <= 2 && count <= MAX_ITERATION);
+
+	return count;
+}
+
+function createProgram(gl, vertexShaderSource, fragmentShaderSource) {
+	var vsh = gl.createShader(gl.VERTEX_SHADER);
+	gl.shaderSource(vsh, vertexShaderSource);
+	gl.compileShader(vsh);
+	if (!gl.getShaderParameter(vsh, gl.COMPILE_STATUS)) {
+		throw "Error in vertex shader:  " + gl.getShaderInfoLog(vsh);
+	}
+
+	var fsh = gl.createShader(gl.FRAGMENT_SHADER);
+	gl.shaderSource(fsh, fragmentShaderSource);
+	gl.compileShader(fsh);
+	if (!gl.getShaderParameter(fsh, gl.COMPILE_STATUS)) {
+		throw "Error in fragment shader:  " + gl.getShaderInfoLog(fsh);
+	}
+
+	var prog = gl.createProgram();
+	gl.attachShader(prog, vsh);
+	gl.attachShader(prog, fsh);
+	gl.linkProgram(prog);
+	if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
+		throw "Link error in program:  " + gl.getProgramInfoLog(prog);
+	}
+
+	return prog;
+}
+
+function map_point(P, Q, A, B, X) {
+	var PX = 0,
+		PQ = 0,
+		PX_dist = 0,
+		PQ_dist = 0,
+		alpha;
+	if (isVector(P)) {
+		for (let i = 0; i < P.length; i++) {
+			PX += (P[i] - X[i]) ** 2;
+
+			PQ += (P[i] - Q[i]) ** 2;
+		}
+
+		PX_dist = Math.sqrt(PX);
+		PQ_dist = Math.sqrt(PQ);
+
+		alpha = PX_dist / PQ_dist;
+	} else {
+		PX_dist = X - P;
+		PQ_dist = Q - P;
+		alpha = PX_dist / PQ_dist;
+	}
+	let Y;
+	Y = mix(A, B, alpha);
+
+	return Y;
+}
+
+function isVector(v) {
+	if (v.type == "vec2" || v.type == "vec3" || v.type == "vec4") {
+		return true;
+	}
+	return false;
+}
+
 window.onload = function init() {
 	var canvas = document.getElementById("set");
 	gl = WebGLUtils.setupWebGL(canvas);
@@ -12,6 +106,8 @@ window.onload = function init() {
 	if (!gl) {
 		alert("WebGL is not available");
 	}
+
+	gl.viewport(0, 0, canvas.width, canvas.height);
 	gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
 	var program = createProgram(gl, vSHADER, fSHADER);
@@ -46,6 +142,9 @@ window.onload = function init() {
 					vec2(0, q)
 				)[1],
 			};
+
+			const escapetime = mandelbrot_set(complex);
+
 			let new_complex = {
 				nx: map_point(
 					vec2(real.start, 0),
@@ -63,7 +162,6 @@ window.onload = function init() {
 				)[1],
 			};
 
-			const escapetime = mandelbrot_set(complex);
 			level.push(vec2(new_complex.nx, new_complex.ny));
 
 			if (escapetime == MAX_ITERATION) {
@@ -96,113 +194,7 @@ window.onload = function init() {
 	render();
 };
 
-function square(z) {
-	// (a + bi)^2 = a^2 - b^2 + 2abi
-	return vec2(z[0] * z[0] - z[1] * z[1], 2 * z[0] * z[1]);
-}
-
-function norm(z) {
-	return Math.sqrt(z[0] ** 2 + z[1] ** 2);
-}
-
-function mandelbrot_set(c) {
-	c = vec2(c.x, c.y);
-	var count = 0,
-		z = vec2(0.0, 0.0);
-	do {
-		z = add(square(z), c);
-		count++;
-	} while (norm(z) <= 2 && count <= MAX_ITERATION);
-
-	return count;
-}
-
-var vSHADER = `
-			attribute vec2 vPosition;
-			attribute vec4 vColor;
-			varying vec4 g1_vColor;
-
-			void main(){
-				gl_Position = vec4(vPosition, 0.0, 1.0);
-				g1_vColor = vColor;
-				gl_PointSize = 1.0;
-			}`,
-	fSHADER = `
-			precision highp float;
-			uniform vec4 g1_vColor;
-			// varying vec4 fragColor;
-	
-			void main()
-			{
-				gl_FragColor = g1_vColor;
-			}`;
-
-function createProgram(gl, vertexShaderSource, fragmentShaderSource) {
-	var vsh = gl.createShader(gl.VERTEX_SHADER);
-	gl.shaderSource(vsh, vertexShaderSource);
-	gl.compileShader(vsh);
-	if (!gl.getShaderParameter(vsh, gl.COMPILE_STATUS)) {
-		throw "Error in vertex shader:  " + gl.getShaderInfoLog(vsh);
-	}
-
-	var fsh = gl.createShader(gl.FRAGMENT_SHADER);
-	gl.shaderSource(fsh, fragmentShaderSource);
-	gl.compileShader(fsh);
-	if (!gl.getShaderParameter(fsh, gl.COMPILE_STATUS)) {
-		throw "Error in fragment shader:  " + gl.getShaderInfoLog(fsh);
-	}
-
-	var prog = gl.createProgram();
-	gl.attachShader(prog, vsh);
-	gl.attachShader(prog, fsh);
-	gl.linkProgram(prog);
-	if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
-		throw "Link error in program:  " + gl.getProgramInfoLog(prog);
-	}
-
-	return prog;
-}
 function render() {
 	gl.clear(gl.COLOR_BUFFER_BIT);
 	gl.drawArrays(gl.POINTS, 0, level.length);
-}
-
-function map_point(P, Q, A, B, X) {
-	/*
-    This Function maps X from PQ to AB
-    where 
-    P, Q = Two points on a line segment
-    X = affine combination of P, Q such that X = αQ + (1 − α)P such that |P X| : |XQ| = α : (1 − α) , 0 ≤ α ≤ 1
-    A, B = colors at P, and Q respectively
-    It returns Y which is the mapping of PQ to AB
-    types:
-    X, P, Q = vec2
-    A, B, Y = vec4
-    */
-	var PX = 0,
-		PQ = 0,
-		PX_dist = 0,
-		PQ_dist = 0;
-	// checks if it's a vector
-	if (isVector(P)) {
-		for (let i = 0; i < P.length; i++) {
-			PX += (P[i] - X[i]) ** 2; //calculates square to calculare distance
-
-			PQ += (P[i] - Q[i]) ** 2; //calculates square to calculare distance
-		}
-
-		PX_dist = Math.sqrt(PX); //distance between P and X
-		PQ_dist = Math.sqrt(PQ); //distance between P and Q
-
-		alpha = PX_dist / PQ_dist;
-	} else {
-		//if it's not a point
-		PX_dist = X - P;
-		PQ_dist = Q - P;
-		alpha = PX_dist / PQ_dist;
-	}
-	//mapped point on the given A B range
-	let Y = mix(A, B, alpha);
-
-	return Y;
 }
